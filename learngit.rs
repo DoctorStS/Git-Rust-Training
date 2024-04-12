2d881e9585a0ca86593399b75cc93f601d0ff5ff
Stanislav Shustov <doctorst.s@gmail.com>
2024-04-12 17:11:09 +0200

    Revert undone

--- learngit.rs
+++ learngit.rs
@@ -1,4 +1,19 @@
 fn main() {
-    println!("Size of a char is {}", std::mem::size_of::<char>());
-    println!("Size of a: {}", "a".len())
+    println!("Size of a char is {}", std::mem::size_of::<char>()); // 4 byte
+    println!("Size of a: {}", "a".len());  // 1 byte
+    /* WOW */
+    let num = {
+        let num2 = 9;
+        num2 + 9
+    };
+    let num3 = num + num;
+    println!("Sum of {num} and {num} is {num3:?}");
+
+    let nulus = ();
+    println!("Woopss {:?}", nulus); // {:#?} for pretty-print
+
+    print!("Max of usize is {}", usize::MAX);
+    println!(" and Min is {}", usize::MIN);
+    print!("Max of isize is {}", isize::MAX);
+    println!(" and Min is {}", isize::MIN);
 }
