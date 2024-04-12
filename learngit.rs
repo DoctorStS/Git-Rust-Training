772101ec05bbe76dde98388234c42f29aadc30c2
Stanislav Shustov <doctorst.s@gmail.com>
2024-04-12 17:03:48 +0200

    Something commited after revert2

--- learngit.rs
+++ learngit.rs
@@ -1,14 +1,5 @@
 fn main() {
     println!("Size of a char is {}", std::mem::size_of::<char>()); // 4 byte
-    println!("Size of a: {}", "a".len());  // 1 byte
+    println!("Size of a: {}", "a".len())  // 1 byte
     /* WOW */
-    let num = {
-        let num2 = 9;
-        num2 + 9
-    };
-    let num3 = num + num;
-    println!("Sum of {num} and {num} is {num3:?}");
-
-    let nulus = ();
-    println!("Woopss {:?}", nulus); // {:#?} for pretty-print
 }
