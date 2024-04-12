75a5d4b93333f171748ee0cb38951e218110cd37
Stanislav Shustov <doctorst.s@gmail.com>
2024-04-12 17:04:52 +0200

    Revert "Comments added"
    This reverts commit 2ed6b7c632a0fae4cb002caf28d4376e9b4488af.

--- learngit.rs
+++ learngit.rs
@@ -1,5 +1,4 @@
 fn main() {
-    println!("Size of a char is {}", std::mem::size_of::<char>()); // 4 byte
-    println!("Size of a: {}", "a".len())  // 1 byte
-    /* WOW */
+    println!("Size of a char is {}", std::mem::size_of::<char>());
+    println!("Size of a: {}", "a".len())
 }
