2ed6b7c632a0fae4cb002caf28d4376e9b4488af
Stanislav Shustov <doctorst.s@gmail.com>
2024-04-12 16:14:29 +0200

    Comments added

--- learngit.rs
+++ learngit.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("Size of a char is {}", std::mem::size_of::<char>());
-    println!("Size of a: {}", "a".len())
+    println!("Size of a char is {}", std::mem::size_of::<char>()); // 4 byte
+    println!("Size of a: {}", "a".len())  // 1 byte
+    /* WOW */
 }
