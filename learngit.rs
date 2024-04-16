ab2823832fe1a7ab8b78beb289191833ebc7df07
Stanislav Shustov <doctorst.s@gmail.com>
2024-04-16 14:25:31 +0200

    Updated for Lesson 6

--- learngit.rs
+++ learngit.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Size of a char is {}", std::mem::size_of::<char>()); // 4 byte
-    println!("Size of a: {}", "a".len());  // 1 byte
+    println!("Size of 'a': {}", "a".len());  // 1 byte
     /* WOW */
     let num = {
         let num2 = 9;
